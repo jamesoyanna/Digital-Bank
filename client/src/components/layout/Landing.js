39356ed6e38c667baa4813870db85f0712e497cd
eshoiza <engrjayt200@gmail.com>
2020-05-31 21:52:55 +0100

    Added a landing page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,10 +1,24 @@
-import React from "react";
-
-const Landing = () => {
-  return (
-    <div>
-      <h4>Hello from Landing</h4>
-    </div>
-  );
-};
+import React, { Component } from 'react'
+import {Link} from "react-router-dom";
+class Landing extends Component {
+    render() {
+        return (
+            <div className="landing">
+              <header>
+                <Link className="logo" to="/">
+                    <h1>
+                     <strong>DIGITAL</strong>BANK
+                    </h1>
+                    </Link>
+                    <button className="form-register-button">
+                        <Link to="/register">Sign up</Link>
+                        </button> 
+                        <button className="form-login-button">
+                            <Link to="/login">Login</Link>
+                            </button> 
+                </header>  
+            </div>
+        );
+    }
+}
 export default Landing;
